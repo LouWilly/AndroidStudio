85b5f9c9cbc77313e7f4e5a56c0f08ad19ebd23a
Luisa Luz <83250828+LouWilly@users.noreply.github.com>
2022-03-28 09:09:41 -0300

    Add files via upload

---
+++ diceroller/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity() : AppCompatActivity(), Parcelable {
+
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            //toast.show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val diceRoll = rollDice()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+ }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MainActivity> {
+        override fun createFromParcel(parcel: Parcel): MainActivity {
+            return MainActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MainActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
